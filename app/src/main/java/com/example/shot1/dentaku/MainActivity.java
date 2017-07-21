d977de809457f46048d9df0a4c9bb62883fc588c
hiro-orbital <hiro.psp@hotmail.co.jp>
2017-07-21 20:55:03 +0900

    ほぼ完成 割り算までできる（すごい）

--- app/src/main/java/com/example/shot1/dentaku/MainActivity.java
+++ app/src/main/java/com/example/shot1/dentaku/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.shot1.dentaku;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
@@ -166,7 +169,11 @@ public class MainActivity extends AppCompatActivity {
                     //計算する
                     Calc ca = new Calc();
                     int a = ca.Interpretation(Shiki);
-                    textView.setText(String.valueOf(a));
+                    int flag=ca.checkWaru();
+                    if (flag==1)
+                        textView.setText("およそ"+String.valueOf(a));
+                    else
+                        textView.setText(String.valueOf(a));
                     Shiki = String.valueOf(a);
                 }
             }
@@ -182,6 +189,27 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //main.xmlの内容を読み込む
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                ShikiOperator= "/";
+                shikiCheck();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 
     private void shikiCheck(){
         if (Shiki.length()>0){
